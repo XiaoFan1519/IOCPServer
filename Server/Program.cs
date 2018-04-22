22c996fc388a757e8d7c597d30042940c05058b7
Fan <xiaofanms@outlook.com>
2018-04-23 00:10:40 +0800

    开始编写。

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,67 @@ namespace Server
 {
     class Program
     {
+
+        Socket listenSocket;
+
         static void Main(string[] args)
         {
+            new Program().Listen(11000);
+        }
+
+        public void Listen(int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse("0.0.0.0");
+
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            // Create a TCP/IP socket.
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Bind the socket to the local endpoint and listen for incoming connections.
+
+            listenSocket.Bind(localEndPoint);
+            listenSocket.Listen(100);
+
+            StartAccept(null);
+        }
+
+        // Begins an operation to accept a connection request from the client 
+        //
+        // <param name="acceptEventArg">The context object to use when issuing 
+        // the accept operation on the server's listening socket</param>
+        public void StartAccept(SocketAsyncEventArgs acceptEventArg)
+        {
+            if (acceptEventArg == null)
+            {
+                acceptEventArg = new SocketAsyncEventArgs();
+                acceptEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(AcceptEventArg_Completed);
+            }
+            else
+            {
+                // socket must be cleared since the context object is being reused
+                acceptEventArg.AcceptSocket = null;
+            }
+
+            bool willRaiseEvent = listenSocket.AcceptAsync(acceptEventArg);
+            if (!willRaiseEvent)
+            {
+                ProcessAccept(acceptEventArg);
+            }
+        }
+
+        // This method is the callback method associated with Socket.AcceptAsync 
+        // operations and is invoked when an accept operation is complete
+        //
+        void AcceptEventArg_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            ProcessAccept(e);
+        }
+
+        private void ProcessAccept(SocketAsyncEventArgs e)
+        {
+            // Get the socket for the accepted client connection and put it into the 
+            //ReadEventArg object user token
+            
         }
     }
 }
