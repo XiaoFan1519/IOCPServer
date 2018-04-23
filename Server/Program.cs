5cf152cfc480c154adbeb4a211082610b7df739e
Fan <xiaofanms@outlook.com>
2018-04-23 23:58:02 +0800

    提交修改。

--- Server/Program.cs
+++ Server/Program.cs
@@ -32,6 +32,9 @@ namespace Server
             listenSocket.Listen(100);
 
             StartAccept(null);
+
+            Console.WriteLine("Press any key to terminate the server process....");
+            Console.ReadKey();
         }
 
         // Begins an operation to accept a connection request from the client 
@@ -66,11 +69,50 @@ namespace Server
             ProcessAccept(e);
         }
 
+        void IO_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            // determine which type of operation just completed and call the associated handler
+            switch (e.LastOperation)
+            {
+                case SocketAsyncOperation.Receive:
+                    ProcessReceive(e);
+                    break;
+                case SocketAsyncOperation.Send:
+                    break;
+                default:
+                    throw new ArgumentException("The last operation completed on the socket was not a receive or send");
+            }
+
+        }
+
         private void ProcessAccept(SocketAsyncEventArgs e)
         {
-            // Get the socket for the accepted client connection and put it into the 
-            //ReadEventArg object user token
-            
+            SocketAsyncEventArgs readEventArgs = new SocketAsyncEventArgs
+            {
+                UserToken = e.AcceptSocket
+            };
+            readEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(IO_Completed);
+            readEventArgs.SetBuffer(new byte[255], 0, 255);
+            // As soon as the client is connected, post a receive to the connection
+            bool willRaiseEvent = e.AcceptSocket.ReceiveAsync(readEventArgs);
+            if (!willRaiseEvent)
+            {
+                ProcessReceive(readEventArgs);
+            }
+
+            // Accept the next connection request
+            StartAccept(e);
+        }
+
+        private void ProcessReceive(SocketAsyncEventArgs e)
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(e.Buffer, e.Offset, e.BytesTransferred));
+            Socket socket = e.UserToken as Socket;
+            bool willRaiseEvent = socket.ReceiveAsync(e);
+            if (!willRaiseEvent)
+            {
+                ProcessAccept(e);
+            }
         }
     }
 }
