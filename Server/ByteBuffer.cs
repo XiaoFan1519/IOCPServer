57a10c225ffec88261fc50977b592a13d44bcfdb
Fan <xiaofanms@outlook.com>
2018-05-01 22:30:28 +0800

    增加需要的方法。

--- Server/ByteBuffer.cs
+++ Server/ByteBuffer.cs
@@ -9,11 +9,39 @@ namespace Server
     class ByteBuffer
     {
         private byte[] buffer;
-
+        
         private int readerIndex;
+        
         private int writerIndex;
 
+        /// <summary>
+        /// 读索引
+        /// </summary>
         public int ReaderIndex => readerIndex;
+
+        /// <summary>
+        /// 写索引
+        /// </summary>
         public int WriterIndex => writerIndex;
+        
+        /// <summary>
+        /// Returns the number of readable bytes which is equal to
+        /// writerIndex - readerIndex
+        /// </summary>
+        /// <returns></returns>
+        public int ReadableBytes()
+        {
+            return writerIndex - readerIndex;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ByteBuffer Clear()
+        {
+            readerIndex = writerIndex = 0;
+            return this;
+        }
     }
 }
