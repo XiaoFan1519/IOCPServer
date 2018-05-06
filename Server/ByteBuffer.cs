7ed50d4b3a18fe66f23b2b64d1303202563e9397
Fan <xiaofanms@outlook.com>
2018-05-06 21:52:16 +0800

    增加扩容和越界检测方法。

--- Server/ByteBuffer.cs
+++ Server/ByteBuffer.cs
@@ -9,6 +9,11 @@ namespace Server
 {
     class ByteBuffer
     {
+        /// <summary>
+        /// 容量
+        /// </summary>
+        private int capacity;
+
         private byte[] buffer;
         
         private int readerIndex;
@@ -25,6 +30,17 @@ namespace Server
         /// </summary>
         public int WriterIndex => writerIndex;
         
+        public ByteBuffer(int capacity)
+        {
+            this.capacity = capacity;
+            buffer = new byte[capacity];
+        }
+
+        public ByteBuffer() : this(10240)
+        {
+            
+        }
+
         /// <summary>
         /// Returns the number of readable bytes which is equal to
         /// writerIndex - readerIndex
@@ -33,6 +49,15 @@ namespace Server
         public int ReadableBytes()
         {
             return writerIndex - readerIndex;
+        }
+
+        /// <summary>
+        /// 获取当前可以写如的字节数
+        /// </summary>
+        /// <returns></returns>
+        public int WritableBytes()
+        {
+            return capacity - writerIndex;
         }
 
         /// <summary>
@@ -46,12 +71,32 @@ namespace Server
         }
 
         /// <summary>
+        /// 确保可写
+        /// </summary>
+        /// <param name="length">长度</param>
+        public void EnsureWritable(int length)
+        {
+            if (length <= WritableBytes())
+            {
+                return;
+            }
+
+            // 扩容
+            int newLength = capacity + length * 2;
+            byte[] buffer = new byte[newLength];
+            Buffer.BlockCopy(this.buffer, 0, buffer, 0, writerIndex);
+            capacity = newLength;
+            this.buffer = buffer;
+        }
+
+        /// <summary>
         /// 将字节数组写入ByteBuffer中
         /// </summary>
         /// <param name="bytes"></param>
         /// <returns></returns>
         public ByteBuffer WriteBytes(byte[] bytes)
         {
+            EnsureWritable(bytes.Length);
             Buffer.BlockCopy(bytes, 0, buffer, writerIndex, bytes.Length);
             writerIndex += bytes.Length;
             return this;
@@ -70,5 +115,48 @@ namespace Server
             return this;
         }
 
+        /// <summary>
+        /// 将int按照网络字节序写入到ByteBuffer中
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public ByteBuffer WriteInt(int value)
+        {
+            value = IPAddress.HostToNetworkOrder(value);
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteBytes(bytes);
+            return this;
+        }
+
+        /// <summary>
+        /// 越界检测
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="capacity"></param>
+        /// <returns></returns>
+        public static bool IsOutOfBounds(int index, int length, int capacity)
+        {
+            return (index | length | (index + length) | (capacity - (index + length))) < 0;
+        }
+
+        /// <summary>
+        /// 在不影响readerIndex的情况下获取字节
+        /// </summary>
+        /// <param name="index">起始位置</param>
+        /// <param name="count">获取数量</param>
+        /// <returns></returns>
+        public byte[] GetBytes(int index, int count)
+        {
+            // 越界检测
+            if (IsOutOfBounds(index, count, capacity))
+            {
+                throw new ArgumentException(string.Format("index:{0} 或 count:{1} 无效", index, count));
+            }
+
+            byte[] buff = new byte[count];
+            Buffer.BlockCopy(buffer, index, buff, 0, count);
+            return buff;
+        }
     }
 }
