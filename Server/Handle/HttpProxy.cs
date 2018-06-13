8262887584988f8eb71a895ac872acca14bdf6d0
Fan <xiaofanms@outlook.com>
2018-06-14 00:15:34 +0800

    尝试编写HttpProxy。

---
+++ Server/Handle/HttpProxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Server.Handle
+{
+    public class HttpProxyHandle : IHandle
+    {
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(UserToken token, ByteBuffer msg)
+        {
+        }
+    }
+}
